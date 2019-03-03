a9affb912630e2ae8d645c0972408b215c4fab73
Vivek Shimpi <vshimpi100@gmail.com>
2019-03-02 19:04:50 -0500

    heroku db permissions

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "vivek1s",
+    password: null,
     database: "burgers_db"
 });
 
